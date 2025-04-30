14d99479180d6090ab133c4138de7c177954dc71
Fesobi Oreofeoluwa <fesobioreofe@gmail.com>
2025-04-30 18:21:51 +0100

    Data-Base Tables Creation

--- Data_Base.sql
+++ Data_Base.sql
@@ -1,9 +1,19 @@
+-- @block
 CREATE DATABASE Wakeerr;
 USE Wakeerr;
 
+-- @block
 CREATE TABLE User_ID (
     User_ID INT PRIMARY KEY,
     User_Name VARCHAR(255) NOT NULL,
     User_Email VARCHAR(255) NOT NULL,
     User_Password VARCHAR(255) NOT NULL
+);
+
+-- @block
+CREATE TABLE Score_Info (
+    User_ID INT PRIMARY KEY,
+    Score INT,
+    FOREIGN KEY (User_ID) REFERENCES User_ID(User_ID),
+    
 );
